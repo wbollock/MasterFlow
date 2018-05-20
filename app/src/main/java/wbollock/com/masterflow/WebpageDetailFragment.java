f92fdd9abee6fceb7a4953d3ec6e75467fd2a137
Will Bollock <wbollock@gmail.com>
2018-05-20 17:39:30 -0400

    Initial commit

---
+++ app/src/main/java/wbollock/com/masterflow/WebpageDetailFragment.java
@@ -0,0 +1,84 @@
+package wbollock.com.masterflow;
+
+import android.app.Activity;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
+import android.webkit.WebView;
+
+
+
+import wbollock.com.masterflow.dummy.DummyContent;
+
+/**
+ * A fragment representing a single Webpage detail screen.
+ * This fragment is either contained in a {@link WebpageListActivity}
+ * in two-pane mode (on tablets) or a {@link WebpageDetailActivity}
+ * on handsets.
+ */
+public class WebpageDetailFragment extends Fragment {
+    /**
+     * The fragment argument representing the item ID that this fragment
+     * represents.
+     */
+    public static final String ARG_ITEM_ID = "item_id";
+
+    /**
+     * The dummy content this fragment is presenting.
+     */
+    private DummyContent.DummyItem mItem;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public WebpageDetailFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments().containsKey(ARG_ITEM_ID)) {
+            // Load the dummy content specified by the fragment
+            // arguments. In a real-world scenario, use a Loader
+            // to load content from a content provider.
+            mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
+
+            Activity activity = this.getActivity();
+            CollapsingToolbarLayout appBarLayout = (CollapsingToolbarLayout) activity.findViewById(R.id.toolbar_layout);
+            if (appBarLayout != null) {
+                appBarLayout.setTitle(mItem.item_name);
+            }
+
+        }
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.webpage_detail, container, false);
+
+        // Show the dummy content as text in a TextView.
+        if(mItem != null){
+
+            ((WebView) rootView.findViewById(R.id.detail_area)).getSettings().setJavaScriptEnabled(true);
+
+            ((WebView) rootView.findViewById(R.id.detail_area)).setLayerType(rootView.LAYER_TYPE_SOFTWARE, null);
+
+            ((WebView) rootView.findViewById(R.id.detail_area)).loadUrl(mItem.url);
+
+            ((WebView) rootView.findViewById(R.id.detail_area)).setWebViewClient(new WebViewClient());
+
+        }
+
+        return rootView;
+    }
+}
